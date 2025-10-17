66af9a59a346affcf4dbe8d631e7753e505dae20
Pranavan <npranavan28@gmail.com>
2025-10-17 12:17:11 +0530

    init

--- config.py
+++
@@ -1,7 +0,0 @@
-# MongoDB local config
-MONGO_URI = "mongodb://localhost:27017/"
-DB_NAME = "product_db"
-COLLECTION_NAME = "products"
-
-# Gemini API
-GEMINI_API_KEY = "AIzaSyD_dIzJW_E9n86cdI3F6FjWJmuQ3Vu8sdM"
